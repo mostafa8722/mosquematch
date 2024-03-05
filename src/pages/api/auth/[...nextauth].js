349d776776b84ca5d1eef8601dc4f2831979e435
mostafa8722 <mostafa.8722@gmail.com>
2024-03-05 15:41:56 +0330

    DEBUG: change next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,25 +1,29 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+
+
+export const authOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-     // redirectUri: "https://www.mosquematch.com/api/auth/callback/google",
-    }),
+      GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID ,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET ,
+      }),
+  
   ],
-  secret: process.env.JWT_SECRET,
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.NEXT_AUTH_SECRET ,
   cookies: {
-    callbackUrl: {
-        name: `__Secure-next-auth.callback-url`,
-        options: {
-            sameSite: "lax",
-            path: "/",
-            httpOnly: true,
-            encode: () => process.env.NEXTAUTH_URL,
-            secure: true,
-        },
-    },
-},
-});
+      callbackUrl: {
+          name: `__Secure-next-auth.callback-url`,
+          options: {
+              sameSite: "lax",
+              path: "/",
+              httpOnly: true,
+              encode: () => nextAuthUrl,
+              secure: true,
+          },
+      },
+  },
+};
+export default NextAuth(authOptions);
